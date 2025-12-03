a4523d440fd6a57ac584a36b22ef342e5a872141
shiva prasad Badugu <shivaprasadbadugu@shivas-MacBook-Pro.local>
2025-12-03 08:21:49 +0530

    created s3 bucket using terraform

--- terraform/project3/main.tf
+++ terraform/project3/main.tf
@@ -7,12 +7,33 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "My-static-website" {
-  bucket = "My-unique-bucket-badugu's"
+resource "aws_s3_bucket" "my_static_website" {
+  bucket = "my-unique-bucket-badugus"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "my-lifecycle-config" {
+  bucket = aws_s3_bucket.my_static_website.id
+
+  rule {
+    id = "expiration"
+    status = "Enabled"
+
+    transition {
+      days = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    expiration {
+      days = 365
+    }
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+  }
 }
 
 resource "aws_s3_bucket_website_configuration" "My-website-config" {
-  bucket  = aws_s3_bucket.My-static-website.id
+  bucket  = aws_s3_bucket.my_static_website.id
 
   index_document {
     suffix = "index.html"
